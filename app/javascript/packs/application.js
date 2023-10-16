899721489371126882f51be478532bb3ded5da4d
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-10-16 18:55:28 +0000

    [update] deleted_turbolinks_for_form_reloads

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
@@ -14,5 +14,5 @@ import "../stylesheets/application";
 import "../swiper.js";
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
