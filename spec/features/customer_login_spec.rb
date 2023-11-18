90cfe6358ccfa74c3c7b3406fed7536794c17ee4
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-18 08:06:12 +0000

    [add] customerモデルのバリデーションテスト（空白/文字制約）

---
+++ spec/features/customer_login_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "CustomerLogins", type: :feature do
+  let!(:customer) { create(:customer) }
+  
+  describe "有効なアカウントで会員がログインできるか" do
+    it "" do
+      
+    end
+  end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  it "会員登録時に有効な内容の場合保存できるか" do
-    expect(FactoryBot.build(:customer)).to be_valid
+RSpec.describe Customer, "モデルに関するテスト", type: :model do
+  let!(:customer) { create(:customer) }
+
+  describe "実際に保存してみる" do
+    it "会員登録時に有効な内容の場合保存できるか" do
+      expect(customer).to be_valid
+    end
+  end
+
+  context "空白のバリデーションチェック" do
+    it "first_nameが空白の場合バリデーションされ、エラーメッセージが返ってくるか" do
+      customer.first_name = nil
+
+      expect(customer).to be_invalid
+      expect(customer.errors[:first_name]).to include("を入力してください") #完全一致
+    end
+  end
+
+  context "文字数のチェック"do
+    it "パスワードに文字数が足りない場合エラーメッセージが出るか" do
+      customer.password = "abcd"
+
+      expect(customer).to be_invalid
+      expect(customer.errors[:password]).to include(/字以上/) #部分一致
+    end
   end
 end
